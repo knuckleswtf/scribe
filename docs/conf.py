1f68f84a50535a65a3861ed04e80d1396a198571
shalvah <diakon.ng@gmail.com>
2020-05-13 22:36:09 +0100

    Fix docs

--- docs/conf.py
+++ docs/conf.py
@@ -14,6 +14,7 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 import recommonmark
+import sphinx_rtd_theme
 from recommonmark.transform import AutoStructify
 
 # -- Project information -----------------------------------------------------
@@ -28,7 +29,7 @@ author = 'Shalvah'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['recommonmark']
+extensions = ['recommonmark', 'sphinx_rtd_theme']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
