72c54dc9bfd8e9f3b79c88f9e25161629d066ffd
shalvah <diakon.ng@gmail.com>
2020-05-16 16:04:31 +0100

    Bugfix: allow Markdown to work in parameter descriptions from annotations

--- resources/views/components/field-details.blade.php
+++ resources/views/components/field-details.blade.php
@@ -1,6 +1,6 @@
-<p>
+
     <code><b>{{ $name }}</b></code>&nbsp; @if($type)<small>{{ $type }}</small>@endif @if(!$required)
         <i>optional</i>@endif
     <br>
     {!! $description !!}
-</p>
+
