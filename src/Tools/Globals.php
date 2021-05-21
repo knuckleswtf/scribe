cfb79117328b76f8bd06c564f759485f3ec2b048
Shalvah <shalvah@users.noreply.github.com>
2021-05-21 13:29:20 +0100

    Update Globals.php

--- src/Tools/Globals.php
+++ src/Tools/Globals.php
@@ -4,7 +4,7 @@ namespace Knuckles\Scribe\Tools;
 
 class Globals
 {
-    public const SCRIBE_VERSION = '2.5.3';
+    public const SCRIBE_VERSION = '2.7.0';
 
     public static $shouldBeVerbose = false;
 
