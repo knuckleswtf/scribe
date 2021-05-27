8335b758a8c6562c3a49e54ca52af7266c4a8514
Shalvah <shalvah@users.noreply.github.com>
2021-05-27 14:33:54 +0100

    Update GenerateDocumentationTest.php

--- tests/GenerateDocumentationTest.php
+++ tests/GenerateDocumentationTest.php
@@ -343,7 +343,7 @@ class GenerateDocumentationTest extends TestCase
         $this->artisan('scribe:generate');
 
         $this->assertFileExists(realpath(__DIR__ . '/../static/docs/index.html'));
-        Utils::deleteDirectoryAndContents('/static/docs');
+        Utils::deleteDirectoryAndContents('static/docs');
     }
 
     /** @test */
