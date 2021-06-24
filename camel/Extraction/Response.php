5583b725d714090a198cf906115860626f537c09
shalvah <diakon.ng@gmail.com>
2021-06-25 00:13:54 +0100

    Hide more headers from sample responses

--- camel/Extraction/Response.php
+++ camel/Extraction/Response.php
@@ -3,7 +3,6 @@
 namespace Knuckles\Camel\Extraction;
 
 
-
 use Knuckles\Camel\BaseDTO;
 
 class Response extends BaseDTO
@@ -21,9 +20,27 @@ class Response extends BaseDTO
         if (is_array($parameters['content'])) {
             $parameters['content'] = json_encode($parameters['content']);
         }
+
+        $hiddenHeaders = [
+            'date',
+            'Date',
+            'etag',
+            'ETag',
+            'last-modified',
+            'Last-Modified',
+            'date',
+            'Date',
+            'content-length',
+            'Content-Length',
+            'connection',
+            'Connection',
+            'x-powered-by',
+            'X-Powered-By',
+        ];
         if (!empty($parameters['headers'])) {
-            unset($parameters['headers']['date']);
-            unset($parameters['headers']['Date']);
+            foreach ($hiddenHeaders as $headerName) {
+                unset($parameters['headers'][$headerName]);
+            }
         }
 
         return parent::__construct($parameters);
