46130e2e52a07115ac5ec63e79a928dc61faf392
Yannik Firre <3316758+YannikFirre@users.noreply.github.com>
2022-01-20 11:15:11 +0100

    ADDED - search id in DB when model instance

--- src/Extracting/Strategies/UrlParameters/GetFromLaravelAPI.php
+++ src/Extracting/Strategies/UrlParameters/GetFromLaravelAPI.php
@@ -69,11 +69,15 @@ class GetFromLaravelAPI extends Strategy
                     $type = $this->normalizeTypeName($typeName);
                     $parameters[$paramName]['type'] = $type;
 
-                    // If the user explicitly set a `where()` constraint, use that to refine examples
-                    $parameterRegex = $endpointData->route->wheres[$paramName] ?? null;
-                    $example = $parameterRegex
-                        ? $this->castToType($this->getFaker()->regexify($parameterRegex), $type)
-                        : $this->generateDummyValue($type);
+                    $example  = optional($argumentInstance::first())->id;
+
+                    if($example === null) {
+                        // If the user explicitly set a `where()` constraint, use that to refine examples
+                        $parameterRegex = $endpointData->route->wheres[$paramName] ?? null;
+                        $example = $parameterRegex
+                            ? $this->castToType($this->getFaker()->regexify($parameterRegex), $type)
+                            : $this->generateDummyValue($type);
+                    }
                     $parameters[$paramName]['example'] = $example;
                 }
             } catch (\Throwable $e) {
