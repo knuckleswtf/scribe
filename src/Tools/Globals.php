05cb8071cde399d5086c4ad18f877ba1d4219f05
shalvah <diakon.ng@gmail.com>
2022-07-08 22:50:10 +0200

    3.34.0

--- src/Tools/Globals.php
+++ src/Tools/Globals.php
@@ -4,7 +4,7 @@ namespace Knuckles\Scribe\Tools;
 
 class Globals
 {
-    public const SCRIBE_VERSION = '3.33.0';
+    public const SCRIBE_VERSION = '3.34.0';
 
     public static bool $shouldBeVerbose = false;
 
