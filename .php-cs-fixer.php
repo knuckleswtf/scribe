ca73349051c0347d8b060c65fae02452a8bcca88
Radek Adamiec <radek@adamiec.it>
2022-12-21 19:49:31 +0100

    Add camel directory to cs fixer

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -3,6 +3,7 @@
 $finder = PhpCsFixer\Finder::create()->in([
     __DIR__ . DIRECTORY_SEPARATOR . 'src',
     __DIR__ . DIRECTORY_SEPARATOR . 'tests',
+    __DIR__ . DIRECTORY_SEPARATOR . 'camel',
 ]);
 
 $config = new PhpCsFixer\Config();
