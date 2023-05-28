7ad83bc02cfbdac2e7b2ce548ca7edbbb1811b1a
shalvah <diakon.ng@gmail.com>
2023-05-28 15:37:46 +0200

    Adjust indentation

--- lang/scribe.php
+++ lang/scribe.php
@@ -10,29 +10,29 @@ return [
         "none" => "This API is not authenticated.",
         "instruction" => [
             "query" => <<<TEXT
-              To authenticate requests, include a query parameter **`:parameterName`** in the request.
-            TEXT,
+                To authenticate requests, include a query parameter **`:parameterName`** in the request.
+                TEXT,
             "body" => <<<TEXT
-              To authenticate requests, include a parameter **`:parameterName`** in the body of the request.
-            TEXT,
+                To authenticate requests, include a parameter **`:parameterName`** in the body of the request.
+                TEXT,
             "query_or_body" => <<<TEXT
-              To authenticate requests, include a parameter **`:parameterName`** either in the query string or in the request body.
-            TEXT,
+                To authenticate requests, include a parameter **`:parameterName`** either in the query string or in the request body.
+                TEXT,
             "bearer" => <<<TEXT
-              To authenticate requests, include an **`Authorization`** header with the value **`"Bearer :placeholder"`**.
-            TEXT,
+                To authenticate requests, include an **`Authorization`** header with the value **`"Bearer :placeholder"`**.
+                TEXT,
             "basic" => <<<TEXT
-              To authenticate requests, include an **`Authorization`** header in the form **`"Basic {credentials}"`**. 
-              The value of `{credentials}` should be your username/id and your password, joined with a colon (:), 
-              and then base64-encoded.
-            TEXT,
+                To authenticate requests, include an **`Authorization`** header in the form **`"Basic {credentials}"`**. 
+                The value of `{credentials}` should be your username/id and your password, joined with a colon (:), 
+                and then base64-encoded.
+                TEXT,
             "header" => <<<TEXT
-              To authenticate requests, include a **`:parameterName`** header with the value **`":placeholder"`**.
-            TEXT,
+                To authenticate requests, include a **`:parameterName`** header with the value **`":placeholder"`**.
+                TEXT,
         ],
         "details" => <<<TEXT
-          All authenticated endpoints are marked with a `requires authentication` badge in the documentation below.
-        TEXT,
+            All authenticated endpoints are marked with a `requires authentication` badge in the documentation below.
+            TEXT,
     ],
 
     "headings" => [
@@ -64,10 +64,10 @@ return [
         "received_response" => "Received response",
         "request_failed" => "Request failed with error",
         "error_help" => <<<TEXT
-          Tip: Check that you're properly connected to the network.
-          If you're a maintainer of ths API, verify that your API is running and you've enabled CORS.
-          You can check the Dev Tools console for debugging information.
-        TEXT,
+            Tip: Check that you're properly connected to the network.
+            If you're a maintainer of ths API, verify that your API is running and you've enabled CORS.
+            You can check the Dev Tools console for debugging information.
+            TEXT,
     ],
 
     "links" => [
