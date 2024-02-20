60ba395c18c67910790374b335c7b0d55a4757e1
Shalvah <diakon.ng@gmail.com>
2024-02-20 12:58:45 +0100

    Revert "4.30.1"
    This reverts commit bab43a04b785e272308c4fc6522a8fc3acc248cc.

--- src/Scribe.php
+++ src/Scribe.php
@@ -9,7 +9,7 @@ use Symfony\Component\HttpFoundation\Request;
 
 class Scribe
 {
-    public const VERSION = '4.30.1';
+    public const VERSION = '4.30.0';
 
     /**
      * Specify a callback that will be executed just before a response call is made
