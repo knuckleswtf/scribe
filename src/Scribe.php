c0afe840081f8c9872b346694a0ab95a0e378842
Shalvah <diakon.ng@gmail.com>
2024-02-29 20:10:57 +0100

    4.33.0

--- src/Scribe.php
+++ src/Scribe.php
@@ -9,7 +9,7 @@ use Symfony\Component\HttpFoundation\Request;
 
 class Scribe
 {
-    public const VERSION = '4.32.0';
+    public const VERSION = '4.33.0';
 
     /**
      * Specify a callback that will be executed just before a response call is made
