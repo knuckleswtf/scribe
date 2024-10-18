76a06e308aba8f14e971c7ec16908bac5f6c9020
geangontijo <64979293+geangontijo@users.noreply.github.com>
2024-10-18 07:12:28 -0300

    Allow parse expressions with Request::validate facade usage (#895)
    * Allow parse expressions with Request::validate facade usage
    
    * fix missing import
    
    * creating tests

---
+++ src/Extracting/Shared/ValidationRulesFinders/RequestValidateFacade.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Knuckles\Scribe\Extracting\Shared\ValidationRulesFinders;
+
+use PhpParser\Node;
+
+/**
+ * This class looks for
+ *   $anyVariable = Request::validate(...);
+ * or just
+ *   Request::validate(...);
+ *
+ * Also supports `->validateWithBag('', ...)`
+ */
+class RequestValidateFacade
+{
+    public static function find(Node $node)
+    {
+        if (!($node instanceof Node\Stmt\Expression)) return;
+
+        $expr = $node->expr;
+        if ($expr instanceof Node\Expr\Assign) {
+            $expr = $expr->expr; // If it's an assignment, get the expression on the RHS
+        }
+
+        if (
+            $expr instanceof Node\Expr\StaticCall
+            && in_array((string) $expr->class, ['Request', \Illuminate\Support\Facades\Request::class])
+        ) {
+            if ($expr->name->name == "validate") {
+                return $expr->args[0]->value;
+            }
+
+            if ($expr->name->name == "validateWithBag") {
+                return $expr->args[1]->value;
+            }
+        }
+    }
+}
