d26ac993dc657efcec039b941d10a7d82a53e85a
olivernybroe <oliver@laravel.com>
2024-11-11 10:50:34 +0100

    fix override generator

--- src/Writing/OpenApiSpecGenerators/OverridesGenerator.php
+++ src/Writing/OpenApiSpecGenerators/OverridesGenerator.php
@@ -9,6 +9,6 @@ class OverridesGenerator extends BaseGenerator
     public function root(array $root, array $groupedEndpoints): array
     {
         $overrides = $this->config->get('openapi.overrides', []);
-        return array_merge($root, $overrides);
+        return array_replace_recursive($root, Arr::undot($overrides));
     }
 }
