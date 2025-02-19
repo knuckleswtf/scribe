f4665c3ed97ab611b00f641993847de0af80cab7
TS1997 <tma.s@hotmail.com>
2025-02-19 19:54:59 +0100

    Updated scalar view to support configuration using html_attributes config

--- resources/views/external/scalar.blade.php
+++ resources/views/external/scalar.blade.php
@@ -1,27 +1,24 @@
 <!doctype html>
 <html>
+
 <head>
     <title>{!! $metadata['title'] !!}</title>
-    <meta charset="utf-8"/>
-    <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1"/>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
     <style>
         body {
             margin: 0;
         }
     </style>
 </head>
+
 <body>
 
-<script
-    id="api-reference"
-@foreach($htmlAttributes as $attribute => $value)
-    {{-- Attributes specified first override later ones --}}
-    {!! $attribute !!}="{!! $value !!}"
-@endforeach
-    data-url="{!! $metadata['openapi_spec_url'] !!}">
-</script>
-<script src="https://cdn.jsdelivr.net/npm/@scalar/api-reference"></script>
+    <script id="api-reference" data-url="{!! $metadata['openapi_spec_url'] !!}"></script>
+    <script>
+        document.getElementById('api-reference').dataset.configuration = JSON.stringify({!! json_encode($htmlAttributes) !!});
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/@scalar/api-reference"></script>
 </body>
+
 </html>
