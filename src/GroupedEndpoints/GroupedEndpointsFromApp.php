693a6066a77c552f7af2c11d907034f1b4bc7aa4
Shalvah <diakon.ng@gmail.com>
2026-01-15 19:13:08 +0100

    cs

--- src/GroupedEndpoints/GroupedEndpointsFromApp.php
+++ src/GroupedEndpoints/GroupedEndpointsFromApp.php
@@ -88,7 +88,7 @@ class GroupedEndpointsFromApp implements GroupedEndpointsContract
     protected function writeEndpointsToDisk(array $grouped): void
     {
         Utils::deleteFilesMatching(static::$camelDir, function ($file) {
-            // @var array|\League\Flysystem\StorageAttributes $file
+            /** @var array|\League\Flysystem\StorageAttributes $file */
             return !Str::startsWith(basename($file['path']), 'custom.');
         });
         Utils::deleteDirectoryAndContents(static::$cacheDir);
