c60c27dd10c967b23ccad352f929de751f023aa7
Greg Miller <gregpmillr@gmail.com>
2026-05-01 17:22:22 -0300

    Fix: Move required from array schema to object schema inside items (#1068)
    * This fix required field placement for any array item schemas in OpenAPI spec
    
    Within the OpenAPI spec, the required array belongs on the object schema
    inside items, not on the parent array schema. Also adds required field
    support for unwrapped array-of-object responses, for example, when Laravel APIs return a collection, it's wrapped in a `data` key like `{ "data": [{"name": "foo"}]}` but you can call `JsonResource::withoutWrapping()` to get an array instead: `[{"name": "foo"}]`. So the bug was that the required field logic existed for the wrapped case, but not the unwrapped branch.
    
    * tests, fix typo, and rename variable for readability
    
    ---------
    
    Co-authored-by: Shalvah <shalvah@users.noreply.github.com>

--- src/Writing/OpenApiSpecGenerators/BaseGenerator.php
+++ src/Writing/OpenApiSpecGenerators/BaseGenerator.php
@@ -230,7 +230,7 @@ class BaseGenerator extends OpenApiGenerator
                     $path
                 );
                 if ($required) {
-                    $schema['required'] = $required;
+                    $schema['items']['required'] = $required;
                 }
             }
         }
@@ -239,7 +239,7 @@ class BaseGenerator extends OpenApiGenerator
     }
 
     /**
-     * Given an enpoint and a set of object keys at a path, return the properties that are specified as required.
+     * Given an endpoint and a set of object keys at a path, return the properties that are specified as required.
      */
     public function filterRequiredResponseFields(OutputEndpointData $endpoint, array $properties, string $path = ''): array
     {
@@ -485,19 +485,27 @@ class BaseGenerator extends OpenApiGenerator
 
                 // Non-empty array
                 if (is_object($decoded[0])) {
-                    // If the first item is an object, we assume it's an array of objects'
+                    // If the first item is an object, we assume it's an array of objects
                     $properties = collect($decoded[0])->mapWithKeys(function ($value, $key) use ($endpoint) {
                         return [$key => $this->generateSchemaForResponseValue($value, $endpoint, $key)];
                     })->toArray();
+
+                    $requiredFields = $this->filterRequiredResponseFields($endpoint, array_keys($properties));
+
+                    $items = [
+                        'type' => $this->convertScribeOrPHPTypeToOpenAPIType(gettype($decoded[0])),
+                        'properties' => $this->objectIfEmpty($properties),
+                    ];
+
+                    if ($requiredFields) {
+                        $items['required'] = $requiredFields;
+                    }
 
                     return [
                         $contentType => [
                             'schema' => [
                                 'type' => 'array',
-                                'items' => [
-                                    'type' => $this->convertScribeOrPHPTypeToOpenAPIType(gettype($decoded[0])),
-                                    'properties' => $this->objectIfEmpty($properties),
-                                ],
+                                'items' => $items,
                                 'example' => $decoded,
                             ],
                         ],
